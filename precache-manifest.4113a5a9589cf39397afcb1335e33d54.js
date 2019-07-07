1269187e468d1fc7522dbd732964100e5499ab82
phirmware <chibuzor.ojukwu@gmail.com>
2019-07-07 14:32:38 +0000

    first

---
+++ precache-manifest.4113a5a9589cf39397afcb1335e33d54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edc94ee9d07bdc491ab240c7fdbae89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aee0b8bc096707a7089",
+    "url": "/static/css/main.bccb73f0.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "5aee0b8bc096707a7089",
+    "url": "/static/js/main.91c288cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
